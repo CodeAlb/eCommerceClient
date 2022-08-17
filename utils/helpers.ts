f1989fe3e746737a19573744e00129a4eda4c34c
CodeAlb <infuzzione@gmail.com>
2022-08-17 22:07:35 +0200

    added storage functions

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,13 +1,17 @@
-export const cn = (...classNames: any) => {
+// Join valid class names.
+export const cn = (...classNames: any[]) => {
   return classNames.filter(Boolean).join(' ')
 }
 
+// Regex callback for Tailwind Syntax
 export const tw = (object: {[key: string]: string}) => object
 
+// Round number by half.
 export const roundByHalf = (num: number) => {
   return Number((Math.round(num * 2) / 2).toFixed(1))
 }
 
+// Calculate total price.
 export const calculateTotal = (items: any[]) => {
   let itemsCount = 0
   let itemsTotal = 0
@@ -23,6 +27,7 @@ export const calculateTotal = (items: any[]) => {
   }
 }
 
+// Fill array.
 export const fillArray = (items: number, value: number = 0) => {
   const itemsAsNum = Number(items)
   if (itemsAsNum > 0) {
@@ -36,4 +41,38 @@ export const numToPrice = (num: number) => {
   const price = Number(num || 0).toFixed(2)
 
   return `${currency}${price}`
+}
+
+export const getLocalStorage = (id: string, defaultValue: any = null) => {
+  try {
+    return JSON.parse(localStorage.getItem(id) as string)
+  } catch {
+    return defaultValue
+  }
+}
+
+export const setLocalStorage = (id: string, value: any) => {
+  try {
+    return localStorage.setItem(id, JSON.stringify(value))
+  } catch {}
+}
+
+export const emptyLocalStorage = (id: string) => {
+  try {
+    return localStorage.removeItem(id)
+  } catch {}
+}
+
+export const getSessionStorage = (id: string, defaultValue: any = null) => {
+  try {
+    return JSON.parse(sessionStorage.getItem(id) as string)
+  } catch {
+    return defaultValue
+  }
+}
+
+export const setSessionStorage = (id: string, value: any) => {
+  try {
+    return sessionStorage.setItem(id, JSON.stringify(value))
+  } catch {}
 }
