55d3ef317468c0e46f1a728e4701b4aaef004e7e
CodeAlb <infuzzione@gmail.com>
2022-08-17 22:07:55 +0200

    added nav links

---
+++ utils/constants.ts
@@ -0,0 +1,20 @@
+export const GUEST_LINKS = [
+  {title: 'Register', path: '/auth/register'},
+  {title: 'Login', path: '/auth/login'},
+]
+
+export const USER_LINKS = [
+  {title: 'My Account', path: '/account'},
+  {title: 'My Orders', path: '/orders'},
+  {title: 'Logout', path: '/logout'},
+]
+
+export const ADMIN_LINKS = [{title: 'Dashboard', path: '/dashboard'}, ...USER_LINKS]
+
+export const DASHBOARD_LINKS = [
+  {title: 'Dashboard', path: '/dashboard'},
+  {title: 'Products', path: '/dashboard/products'},
+  {title: 'Orders', path: '/dashboard/orders'},
+  {title: 'Users', path: '/dashboard/users'},
+  {title: 'Reviews', path: '/dashboard/reviews'},
+]
