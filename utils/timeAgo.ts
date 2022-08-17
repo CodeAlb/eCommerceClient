0d532800ce3bc17b80882336886fa9cfe1658ea0
CodeAlb <infuzzione@gmail.com>
2022-08-17 22:08:09 +0200

    cleanup code

--- utils/timeAgo.ts
+++ utils/timeAgo.ts
@@ -1,20 +1,9 @@
 import TimeAgo, {DateInput} from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en.json'
 
-/* Typescript.
- ********************************/
-
-type Time = DateInput
-
-/* Initializers.
- ********************************/
-
 TimeAgo.addLocale(en)
 TimeAgo.setDefaultLocale('en')
 
-/* Utilities.
- ********************************/
-
-export const timeAgo = (time: Time) => {
+export const timeAgo = (time: DateInput) => {
   return new TimeAgo('en').format(new Date(time))
 }
