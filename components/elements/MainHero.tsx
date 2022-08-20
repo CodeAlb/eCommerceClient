7799238aa7204e41a6eb71afd6ce62f1cf51cdb5
CodeAlb <infuzzione@gmail.com>
2022-08-20 03:27:36 +0200

    extended image

--- components/elements/MainHero.tsx
+++ components/elements/MainHero.tsx
@@ -5,11 +5,11 @@ interface MainHeroProps {}
 
 const css = {
   wrapper: 'max-w-site mx-auto px-4',
-  inner: 'relative rounded-xl overflow-hidden pt-[40%] md:pt-0 md:h-80 lg:h-96',
+  inner: 'relative rounded-xl overflow-hidden pt-[40%] md:pt-0 md:h-96 lg:h-[450px]',
   image: 'absolute inset-0 w-full h-full object-cover',
   overlay: 'absolute inset-0 bg-gradient-to-t md:bg-gradient-to-r from-black opacity-60',
   body: ' relative px-4 py-8 flex flex-col justify-end sm:w-2/3 md:absolute md:left-0 md:top-0 md:p-14 md:h-full',
-  title: 'text-3xl sm:text-4xl lg:text-5xl font-bold text-white',
+  title: 'text-3xl sm:text-4xl lg:text-6xl font-bold text-white',
   action: 'mt-6 sm:mt-8',
   link: 'rounded inline-flex group items-center justify-center bg-white/5 border-2 border-white px-4 h-10 sm:px-6 sm:h-12 text-white font-medium uppercase text-xs sm:text-sm tracking-wider hover:bg-white hover:text-black duration-150',
   icon: 'w-5 ml-2 -mr-1 group-hover:ml-4 duration-150',
@@ -29,7 +29,7 @@ const MainHero = ({}: MainHeroProps) => {
           <div className={css.action}>
             <Link href="/shop">
               <a className={css.link}>
-                Our Shop <ArrowNarrowRightIcon className={css.icon} />
+                Explore More <ArrowNarrowRightIcon className={css.icon} />
               </a>
             </Link>
           </div>
