3d602b047b513306d3bc74b1007654b426bb6d0a
CodeAlb <infuzzione@gmail.com>
2022-08-21 12:11:58 +0200

    added section

---
+++ components/elements/AboutSection.tsx
@@ -0,0 +1,37 @@
+import {cn} from '../../utils/helpers'
+
+const css = {
+  wrapper: 'grid gap-8 md:grid-cols-2 md:gap-16 md:items-center lg:gap-20',
+  subtitle: 'text-sm uppercase font-medium mb-1 sm:mb-2 tracking-widest text-gray-500',
+  title: 'text-2xl sm:text-3xl md:text-4xl font-medium',
+  body: '',
+  mediaReversed: 'md:order-2',
+  content: 'mt-6 sm:mt-8',
+  media: 'relative pb-[60%] md:pb-[90%] overflow-hidden',
+  image: 'absolute inset-0 w-full h-full object-cover rounded-md',
+}
+
+interface AboutSectionProps {
+  title: string
+  subtitle?: string
+  image?: string
+  content: string
+  isReversed?: boolean
+}
+
+const AboutSection = ({title, subtitle, image, content, isReversed}: AboutSectionProps) => {
+  return (
+    <div className={css.wrapper}>
+      <div className={cn(css.media, isReversed && css.mediaReversed)}>
+        {image && <img src={image} alt={title} className={css.image} />}
+      </div>
+      <div className={css.body}>
+        {subtitle && <p className={css.subtitle}>{subtitle}</p>}
+        <h3 className={css.title}>{title}</h3>
+        <div className={css.content} dangerouslySetInnerHTML={{__html: content}} />
+      </div>
+    </div>
+  )
+}
+
+export default AboutSection
