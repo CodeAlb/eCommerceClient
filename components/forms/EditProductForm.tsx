d0ba8ff35dc5af2462bf6262373d4866e5444653
CodeAlb <infuzzione@gmail.com>
2022-08-24 20:49:21 +0200

    added images

--- components/forms/EditProductForm.tsx
+++ components/forms/EditProductForm.tsx
@@ -1,10 +1,13 @@
-import {useEffect} from 'react'
+import {TrashIcon, XIcon} from '@heroicons/react/outline'
+import {useEffect, useState} from 'react'
 import {useForm} from 'react-hook-form'
 import {toast} from 'react-toastify'
 import {useUpdateProductMutation} from '../../store/api/baseApi'
+import {IImage} from '../../types/image'
 import {IProduct} from '../../types/product'
 import {PRODUCT_CATEGORIES, PRODUCT_SELLERS} from '../../utils/constants'
 import {cn} from '../../utils/helpers'
+import Images from '../fields/Images'
 import Input from '../fields/Input'
 import Select from '../fields/Select'
 import Textarea from '../fields/Textarea'
@@ -22,10 +25,26 @@ const css = {
     'rounded inline-flex items-center px-6 h-10 sm:px-8 sm:h-12 font-medium uppercase text-xs sm:text-sm tracking-wider duration-150',
   submitDisabled: 'bg-gray-300 text-white',
   submitNormal: 'bg-black text-white hover:bg-gray-900',
+  images: 'grid grid-cols-4 gap-2',
+  imageItem: 'relative pt-[80%]',
+  image: 'absolute inset-0 w-full h-full object-cover',
+  deleteBtn: 'group rounded-full bg-white absolute top-0.5 right-0.5 p-1 shadow-md',
+  deleteIcon: 'w-5 text-gray-500 group-hover:text-red-600 duration-150',
 }
 
 const EditProductForm = ({product}: EditProductFormProps) => {
-  const {_id, name, price, oldPrice, description, excerpt, stock, seller, category} = product
+  const {
+    _id,
+    name,
+    price,
+    oldPrice,
+    description,
+    excerpt,
+    stock,
+    seller,
+    images: productImages,
+    category,
+  } = product
   const {
     register,
     handleSubmit,
@@ -44,6 +63,12 @@ const EditProductForm = ({product}: EditProductFormProps) => {
     },
   })
   const [updateProduct, {isLoading, isSuccess, isError, error, data}] = useUpdateProductMutation()
+  const [images, setImages] = useState(productImages || [])
+  const [deletedImages, setDeletedImages] = useState<IImage[]>([])
+
+  useEffect(() => {
+    setImages(productImages as IImage[])
+  }, [productImages])
 
   useEffect(() => {
     if (isSuccess) {
@@ -54,9 +79,44 @@ const EditProductForm = ({product}: EditProductFormProps) => {
     }
   }, [isSuccess, isError, error, data])
 
+  const onImageDelete = (index: number) => {
+    const foundImage = images[index]
+    const isPreviewImage = typeof foundImage === 'string'
+    if (!isPreviewImage) {
+      setDeletedImages((state) => [...state, foundImage as IImage])
+    }
+
+    setImages((state) => {
+      let newArray = [...state]
+      newArray.splice(index, 1)
+      return newArray
+    })
+  }
+
+  console.log(deletedImages)
+
   const sendFormData = (data: any) => {
     if (!isLoading) {
-      updateProduct({_id, ...data})
+      const formData = new FormData()
+
+      formData.set('name', data.name)
+      formData.set('price', data.price)
+      if (data.oldPrice) {
+        formData.set('oldPrice', data.oldPrice)
+      }
+      formData.set('description', data.description)
+      formData.set('excerpt', data.excerpt)
+      formData.set('stock', data.stock)
+      formData.set('seller', data.seller)
+      formData.set('category', data.category)
+
+      images.forEach((image) => {
+        formData.append('images', JSON.stringify(image))
+      })
+      deletedImages.forEach((image) => {
+        formData.append('deletedImages', image.public_id)
+      })
+      updateProduct({_id, formData})
     }
   }
 
@@ -173,6 +233,23 @@ const EditProductForm = ({product}: EditProductFormProps) => {
           }}
           errors={errors}
         />
+        <Images setImages={setImages} />
+        <div className={css.images}>
+          {images.map((image: IImage | string, i) => (
+            <div key={i} className={css.imageItem}>
+              <img src={typeof image === 'string' ? image : image?.url} className={css.image} />
+              <button
+                type="button"
+                onClick={() => {
+                  onImageDelete(i)
+                }}
+                className={css.deleteBtn}
+              >
+                <TrashIcon strokeWidth={1.4} className={css.deleteIcon} />
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
       <div className={css.action}>
         <button
