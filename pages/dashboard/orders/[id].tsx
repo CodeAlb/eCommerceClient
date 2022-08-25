306cf66059029d2ac8df51d9a21b61cb0db4b7fb
CodeAlb <infuzzione@gmail.com>
2022-08-25 03:29:40 +0200

    added delete order

--- pages/dashboard/orders/[id].tsx
+++ pages/dashboard/orders/[id].tsx
@@ -1,9 +1,14 @@
 import {GetStaticPaths, GetStaticProps} from 'next'
-import {useGetOrderQuery} from '../../../store/api/baseApi'
+import {useDeleteOrderMutation, useGetOrderQuery} from '../../../store/api/baseApi'
 import {NextSeo} from 'next-seo'
 import {timeAgo} from '../../../utils/timeAgo'
 import {numToPrice} from '../../../utils/helpers'
 import Image from 'next/future/image'
+import OrderForm from '../../../components/forms/OrderForm'
+import {useEffect, useState} from 'react'
+import {useRouter} from 'next/router'
+import {toast} from 'react-toastify'
+import {DIR_PATHS} from '../../../utils/constants'
 
 type OrderPageProps = {
   id: string
@@ -11,7 +16,9 @@ type OrderPageProps = {
 
 const css = {
   main: '',
-  title: 'text-xl sm:text-2xl md:text-3xl uppercase font-medium mb-6 sm:mb-8 md:mb-10',
+  headline: 'mb-6 sm:mb-8 md:mb-10 flex items-center',
+  title: 'text-xl sm:text-2xl md:text-3xl uppercase font-medium',
+  action: 'ml-8 relative',
   order: 'max-w-3xl space-y-8 sm:space-y-10',
   section: 'p-4 sm:p-6 md:p-8 bg-white shadow border border-gray-200 rounded',
   sectionTitle:
@@ -29,21 +36,70 @@ const css = {
   productBody: 'flex flex-col overflow-x-hidden',
   productName: 'font-medium truncate',
   productMeta: 'text-sm',
+  deleteBtn:
+    'bg-red-50 text-red-700 rounded px-2 py-1 sm:px-3 sm:py-1.5 text-xs uppercase font-medium hover:bg-red-100 duration-150',
+  cancelBtn:
+    'bg-gray-50 text-gray-600 rounded px-2 py-1 sm:px-3 sm:py-1.5 text-xs uppercase font-medium hover:bg-gray-100 duration-150',
+  drop: 'absolute right-0 top-0 shadow-lg bg-white p-4 rounded',
+  buttons: 'flex items-center space-x-2 mt-2',
 }
 
 const OrderPage = ({id}: OrderPageProps) => {
   const {data, isLoading, isFetching} = useGetOrderQuery(id)
-  const showLoader = isLoading || isFetching
+  // const showLoader = isLoading || isFetching
   const {order} = data || {}
   const {_id, paymentInfo, paidAt, orderStatus, user, shippingInfo, totalPrice, orderItems} =
     order || {}
   const title = _id ? `Order #${_id}` : 'Order'
 
+  const [isDeleting, setIsDeleting] = useState(false)
+
+  const {push} = useRouter()
+  const [deleteOrder, {isLoading: isDeleteLoading, isSuccess, isError, error}] =
+    useDeleteOrderMutation()
+
+  useEffect(() => {
+    if (isSuccess) {
+      toast.success('Product was deleted')
+      push(`${DIR_PATHS.dashboard}/orders`)
+    }
+    if (isError) {
+      toast.error((error as any)?.data?.message)
+    }
+  }, [isSuccess, isError, error])
+
+  const onDeleteClick = () => {
+    if (!isDeleteLoading) {
+      deleteOrder(id)
+    }
+  }
+
   return (
     <div>
       <NextSeo title={title} />
-      <h1 className={css.title}>Order Details</h1>
+      <div className={css.headline}>
+        <h1 className={css.title}>Order Details</h1>
+        <div className={css.action}>
+          <button className={css.deleteBtn} onClick={() => setIsDeleting(true)}>
+            Delete
+          </button>
+          {isDeleting && (
+            <div className={css.drop}>
+              Are you sure?
+              <div className={css.buttons}>
+                <button className={css.cancelBtn} onClick={() => setIsDeleting(false)}>
+                  Cancel
+                </button>
+                <button className={css.deleteBtn} onClick={onDeleteClick}>
+                  Delete
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
       <div className={css.main}>
+        {order && <OrderForm order={order} />}
         <div className={css.order}>
           <div className={css.section}>
             <h2 className={css.sectionTitle}>Order Info</h2>
