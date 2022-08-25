9bc2b160a795f915e0c7c9b99f6a70ba4ff48f20
CodeAlb <infuzzione@gmail.com>
2022-08-25 02:56:57 +0200

    added modal when deleting

--- pages/dashboard/products/[id].tsx
+++ pages/dashboard/products/[id].tsx
@@ -4,7 +4,7 @@ import {NextSeo} from 'next-seo'
 import EditProductForm from '../../../components/forms/EditProductForm'
 import {IProduct} from '../../../types/product'
 import EditProductLoader from '../../../components/loaders/EditProductLoader'
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import {DIR_PATHS} from '../../../utils/constants'
 import {useRouter} from 'next/router'
 import {toast} from 'react-toastify'
@@ -16,14 +16,20 @@ type ProductPageProps = {
 const css = {
   headline: 'mb-6 sm:mb-8 md:mb-10 flex items-center',
   title: 'text-xl sm:text-2xl md:text-3xl uppercase font-medium',
+  action: 'ml-8 relative',
   deleteBtn:
-    'ml-8 bg-red-50 text-red-700 rounded px-2 py-1 sm:px-3 sm:py-1.5 text-xs uppercase font-medium hover:bg-red-100 duration-150',
+    'bg-red-50 text-red-700 rounded px-2 py-1 sm:px-3 sm:py-1.5 text-xs uppercase font-medium hover:bg-red-100 duration-150',
+  cancelBtn:
+    'bg-gray-50 text-gray-600 rounded px-2 py-1 sm:px-3 sm:py-1.5 text-xs uppercase font-medium hover:bg-gray-100 duration-150',
+  drop: 'absolute right-0 top-0 shadow-lg bg-white p-4 rounded',
+  buttons: 'flex items-center space-x-2 mt-2',
 }
 
 const ProductPage = ({id}: ProductPageProps) => {
   const {data, isLoading} = useGetProductQuery(id)
   const {product} = data || {}
   const {name} = product || {}
+  const [isDeleting, setIsDeleting] = useState(false)
   const title = name ? `Edit: ${name}` : 'Edit Product'
 
   const {push} = useRouter()
@@ -51,9 +57,24 @@ const ProductPage = ({id}: ProductPageProps) => {
       <NextSeo title={title} />
       <div className={css.headline}>
         <h1 className={css.title}>Edit Product</h1>
-        <button className={css.deleteBtn} onClick={onDeleteClick}>
-          Delete
-        </button>
+        <div className={css.action}>
+          <button className={css.deleteBtn} onClick={() => setIsDeleting(true)}>
+            Delete
+          </button>
+          {isDeleting && (
+            <div className={css.drop}>
+              Are you sure?
+              <div className={css.buttons}>
+                <button className={css.cancelBtn} onClick={() => setIsDeleting(false)}>
+                  Cancel
+                </button>
+                <button className={css.deleteBtn} onClick={onDeleteClick}>
+                  Delete
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
       <div>
         {isLoading || isDeleteLoading ? (
